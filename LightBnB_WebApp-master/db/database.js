97cdc0e2da013aaf02d55ba63c33a14ed1283888
Lauren <laurenkaitlyn@gmail.com>
2023-05-18 13:50:32 -0400

    feat: access users with sql

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,15 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'laurengreasley',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+
+
 
 /// Users
 
@@ -9,14 +19,17 @@ const users = require("./json/users.json");
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE users.email = $1
+  `;
+
+  return pool 
+  .query(queryString, [email])
+  .then(result => result.rows[0])
+  .catch(err => err.message)
+
 };
 
 /**
@@ -25,7 +38,16 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1
+  `;
+
+  return pool
+  .query(queryString, [id])
+  .then(result => result.rows[0])
+  .catch(err => err.message)
 };
 
 /**
@@ -34,10 +56,16 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+  INSERT INTO users (name, password, email)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  return pool 
+  .query(queryString, [user.name, user.password, user.email])
+  .then(result => result.rows)
+  .catch(err => err.message)
 };
 
 /// Reservations
@@ -60,11 +88,15 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  const queryString = `
+  SELECT title
+  FROM properties
+  LIMIT $1
+  `;
+  return pool
+    .query(queryString, [limit])
+    .then(result => result.rows)
+    .catch(err => err.message)
 };
 
 /**
